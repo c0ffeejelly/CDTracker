c1a76bcf992bccd70108e99d02c83d4376aa901b
0moeller <leonie.moeller@studium.uni-hamburg.de>
2025-07-04 23:11:15 +0200

    Add backend project

---
+++ backend/src/main/java/com/coffeejelly/cdtracker/CdtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.coffeejelly.cdtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdtrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/coffeejelly/cdtracker/CdtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeejelly.cdtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
