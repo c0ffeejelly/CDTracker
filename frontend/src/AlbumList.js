52e23e404225715da1cd3fd8d807c00d50ece733
0moeller <leonie.moeller@studium.uni-hamburg.de>
2025-07-05 13:58:24 +0200

    create ALbumList component and simple display of albums

---
+++ frontend/src/AlbumList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+
+const AlbumList = () => {
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/albums')
+        .then(response => setAlbums(response.data))
+        .catch(error => console.error("Error fetching albums: ", error));
+    }, []);
+
+    return (
+    <div>
+        <h2> Albums </h2>
+        <ul>
+            {albums.map(album => (
+            <li key={album.id}>
+            {album.title} by {album.artist}
+            </li>
+            ))}
+        </ul>
+    </div>
+    )
+};
+
+export default AlbumList;
