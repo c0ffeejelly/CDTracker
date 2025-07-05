e28acacc8b62e800ed1d97c01561ae41215becb5
0moeller <leonie.moeller@studium.uni-hamburg.de>
2025-07-05 16:14:22 +0200

    DiscogsController for album search

--- backend/src/main/java/com/coffeejelly/cdtracker/discogs/DiscogsController.java
+++ backend/src/main/java/com/coffeejelly/cdtracker/discogs/DiscogsController.java
@@ -1,2 +1,36 @@
-package com.coffeejelly.cdtracker.discogs;public class DiscogsController {
+package com.coffeejelly.cdtracker.discogs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+@RequestMapping("/api/discogs")
+
+public class DiscogsController {
+
+    @Value("${discogs.token}")
+    private String discogsToken;
+
+    @GetMapping("/search")
+    public ResponseEntity<String> searchAlbums(@RequestParam String query){
+        String url = "https://api.discogs.com/database/search?q=" + URLEncoder.encode(query, StandardCharsets.UTF_8)
+             + "&type=release";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Discogs token=" + discogsToken);
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        return response;
+    }
 }
